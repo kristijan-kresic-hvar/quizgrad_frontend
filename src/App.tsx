0da58c6044c2b40ba791dc1ae2cc059755b6a3d7
kristijan-kresic-hvar <kristijan.kresic.dev@gmail.com>
2023-01-20 20:36:06 +0100

    Initial router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
 import Layout from './containers/Layout';
 import Navbar from './components/Navbar';
 
 const App = () => {
 	return (
 		<Layout navbar={<Navbar />}>
-			<div>App</div>
+			<Outlet />
 		</Layout>
 	);
 };
